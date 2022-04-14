21f6d21d91f376af49d0ca11116f3a7d568ee606
HongPhuc269 <nguyennhuhongphuc269@gmail.com>
2022-04-14 10:12:15 +0700

    Initial file

---
+++ src/main/java/edu/hanu/fit/se2_ecommerce/Se2EcommerceApplication.java
@@ -0,0 +1,13 @@
+package edu.hanu.fit.se2_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se2EcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hanu/fit/se2_ecommerce/Se2EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hanu.fit.se2_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
